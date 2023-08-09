218da3b6513bde13cc6025f12322a63cb824663b
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2023-08-09 13:33:05 +0200

    Show events on welcome page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -16,20 +15,11 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-gray-100">
             @include('layouts.main-navigation')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow dark:bg-gray-800">
-                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
             <!-- Page Content -->
-            <main>
+            <main class="mx-auto mt-4 max-w-7xl">
                 {{ $slot }}
             </main>
         </div>
